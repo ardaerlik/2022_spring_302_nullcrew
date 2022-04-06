9233d2aceb81e3cbd7704ec0a64cb416acf18999
Arda Erlik <aerlik20@ku.edu.tr>
2022-04-06 12:54:07 +0300

    create Ball model class and add creator method to GameObjectFactory

--- src/com/nullcrew/Models/GameObjectFactory.java
+++ src/com/nullcrew/Models/GameObjectFactory.java
@@ -5,5 +5,9 @@ public class GameObjectFactory {
 	public static Paddle createPaddle() {
 		return new Paddle(100, 470, 120, 10);
 	}
+	
+	public static Ball createBall() {
+		return new Ball(10, 10, 10, 10);
+	}
 
 }
