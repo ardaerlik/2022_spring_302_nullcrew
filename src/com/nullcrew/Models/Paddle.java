b9e9b1b4dcd2da58f854e8c94a59af21beec19c7
atakanozkan <atakanozkan@172.20.33.69>
2022-04-06 21:00:32 +0300

    I am commiting on Main.

--- src/com/nullcrew/Models/Paddle.java
+++ src/com/nullcrew/Models/Paddle.java
@@ -3,7 +3,7 @@ package com.nullcrew.Models;
 public class Paddle extends GameObject {
 	
 	private int rotationDegree;
-
+	
 	public Paddle(int x, int y, int width, int height) {
 		super(x, y, width, height);
 		rotationDegree = 0;
