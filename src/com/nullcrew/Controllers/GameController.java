7ee06db1bfcb0c3d6a6c1346988cbd34e8b1365c
Arda Erlik <aerlik20@ku.edu.tr>
2022-04-06 12:47:41 +0300

    add move the paddle use case delegate implementations to GameController

--- src/com/nullcrew/Controllers/GameController.java
+++ src/com/nullcrew/Controllers/GameController.java
@@ -1,5 +1,49 @@
 package com.nullcrew.Controllers;
 
-public class GameController {
+import com.nullcrew.Models.*;
+import com.nullcrew.Utilities.*;
+import com.nullcrew.Views.*;
 
+public class GameController {
+	private GameView gameView;
+	private Paddle paddle;
+	
+	public GameController(GameView gameView) {
+		this.gameView = gameView;
+	}
+
+	public void paddleMoved(MoveDirection direction) {
+		switch (direction) {
+		case RIGHT: {
+			paddle.setX(paddle.getX() + 5);
+			break;
+		}
+		case LEFT: {
+			paddle.setX(paddle.getX() - 5);
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + direction);
+		}
+	}
+	
+	public void paddleRotated(MoveDirection direction) {
+		// TODO: call paddle model and change rotations
+	}
+
+	public GameView getGameView() {
+		return gameView;
+	}
+
+	public void setGameView(GameView gameView) {
+		this.gameView = gameView;
+	}
+
+	public Paddle getPaddle() {
+		return paddle;
+	}
+
+	public void setPaddle(Paddle paddle) {
+		this.paddle = paddle;
+	}
 }
