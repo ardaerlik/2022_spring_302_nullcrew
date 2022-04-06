6729fab28451748f52733492591e5e3ff7abb071
Arda Erlik <aerlik20@ku.edu.tr>
2022-04-06 12:00:04 +0300

    add GameObject abstract class and create Paddle subclass

---
+++ src/com/nullcrew/Models/Paddle.java
@@ -0,0 +1,10 @@
+package com.nullcrew.Models;
+
+public class Paddle extends GameObject {
+
+	public Paddle(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		// TODO Auto-generated constructor stub
+	}
+
+}
