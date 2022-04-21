29502c69291f549201a5d4e6063ec637aab16499
MUHAMMED ALI KERDIGE <mkerdige19@ku.edu.tr>
2022-04-21 03:57:53 +0300

    Add features on MenuView

--- src/com/nullcrew/Views/MenuView.java
+++ src/com/nullcrew/Views/MenuView.java
@@ -3,6 +3,11 @@ package com.nullcrew.Views;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
 
 public class MenuView {
 
@@ -35,9 +40,20 @@ public class MenuView {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SpringLayout springLayout = new SpringLayout();
+		frame.getContentPane().setLayout(springLayout);
+		
+		JPanel panel = new JPanel();
+		springLayout.putConstraint(SpringLayout.NORTH, panel, 0, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, panel, 0, SpringLayout.WEST, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, panel, 768, SpringLayout.NORTH, frame.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, panel, 1024, SpringLayout.WEST, frame.getContentPane());
+		panel.setBackground(Color.PINK);
+		frame.getContentPane().add(panel);
+		frame.setResizable(false);
 	}
-
 }
