da0c3ca06dc823b461817abc05734e57d26277f1
MUHAMMED ALI KERDIGE <mkerdige19@ku.edu.tr>
2022-05-01 22:56:20 +0300

    Added some changes on User Model

--- src/com/nullcrew/Domain/Models/Game.java
+++ src/com/nullcrew/Domain/Models/Game.java
@@ -1,25 +1,22 @@
 package com.nullcrew.Domain.Models;
 
-import java.util.List;
+import java.util.ArrayList;
+
 
 public class Game  {
 	
-	int numOfAsteroids;
 	
-	List<Asteroid> list_of_asteroids;
+	ArrayList<Asteroid> list_of_asteroids;
 	
 	int score;
 	
 	int lives;
 	
-	List<PowerUp> list_of_powerups;
+	ArrayList<PowerUp> list_of_powerups;
 	
-	int numOfPowerups;
 	
-	List<Alien> list_of_aliens;
-	
-	int numOfAliens;
-	
+	ArrayList<Alien> list_of_aliens;
+		
 	
 
 }
