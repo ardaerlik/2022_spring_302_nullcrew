fe1cad27bad530a6aae85cc2e426ebec84f801c1
Arda Erlik <aerlik20@ku.edu.tr>
2022-05-02 03:20:58 +0300

    change mongodb version - jar / implement insert function

--- app/src/main/java/com/nullcrew/Utilities/DBManager.java
+++ app/src/main/java/com/nullcrew/Utilities/DBManager.java
@@ -1,16 +1,19 @@
 package com.nullcrew.Utilities;
 
+import org.bson.Document;
+
 import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerApi;
 import com.mongodb.ServerApiVersion;
-import com.mongodb.reactivestreams.client.MongoClient;
-import com.mongodb.reactivestreams.client.MongoClients;
-import com.mongodb.reactivestreams.client.MongoDatabase;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
 
 public final class DBManager {
 	private static DBManager instance = new DBManager();
-	private static MongoClient mongoClient;
+	private MongoClient client;
+	private MongoDatabase database;
 
 	public static DBManager getInstance() {
 		if (instance == null) {
@@ -24,19 +27,28 @@ public final class DBManager {
 	}
 	
 	public void connectDB() {
-		ConnectionString connectionString = new ConnectionString("mongodb+srv://nullcrew:<NullCrew2022>@cluster0.wcrf4.mongodb.net/alien_asteroid_game?retryWrites=true&w=majority");
+		ConnectionString connectionString = new ConnectionString("mongodb+srv://nullcrew:NullCrew2022@cluster0.wcrf4.mongodb.net/alien_asteroid_game?retryWrites=true&w=majority");
 		MongoClientSettings settings = MongoClientSettings.builder()
 		        .applyConnectionString(connectionString)
 		        .serverApi(ServerApi.builder()
 		            .version(ServerApiVersion.V1)
 		            .build())
 		        .build();
-		mongoClient = MongoClients.create(settings);
-		MongoDatabase database = mongoClient.getDatabase("alien_asteroid_game");
+		client = MongoClients.create(settings);
+		database = client.getDatabase("alien_asteroid_game");
 	}
 	
 	public void closeDB() {
-		mongoClient.close();
+		client.close();
+	}
+	
+	public void registerUser(String email, String password) {
+		Document tmp = new Document("aaa", "aa.")
+				.append("email", email)
+				.append("password", password);
+		
+		database.getCollection("users_test").insertOne(tmp);
+		closeDB();
 	}
 
 }
