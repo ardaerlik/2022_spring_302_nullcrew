4b1c9095fdcfff03162e75b1acd8283610aca31d
Arda Erlik <aerlik20@ku.edu.tr>
2022-05-03 15:42:40 +0300

    create LoginController and MenuController

---
+++ app/src/main/java/com/nullcrew/Domain/Controllers/LoginController.java
@@ -0,0 +1,12 @@
+package com.nullcrew.Domain.Controllers;
+
+import com.nullcrew.AlienAsteroidGame;
+import com.nullcrew.UI.Views.LoginView;
+
+public class LoginController extends AppController {
+
+	public LoginController(LoginView loginView, AlienAsteroidGame app) {
+		super(loginView, app);
+	}
+
+}
