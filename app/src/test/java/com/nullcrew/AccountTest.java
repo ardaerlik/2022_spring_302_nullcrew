070ba844f0681f11129e7a247b8b30580d30553d
Arda Erlik <aerlik20@ku.edu.tr>
2022-05-22 23:01:25 +0300

    add test4 and test5 for AccountTest and succesfully run it

--- app/src/test/java/com/nullcrew/AccountTest.java
+++ app/src/test/java/com/nullcrew/AccountTest.java
@@ -2,16 +2,12 @@ package com.nullcrew;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.bson.types.ObjectId;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import com.nullcrew.Domain.Models.Account;
-import com.nullcrew.Utilities.DBManager;
+import com.nullcrew.Domain.Models.Game;
 
 public class AccountTest {
 	Account account;
@@ -45,13 +41,28 @@ public class AccountTest {
 	@DisplayName("addGame throws NullPointerException when game == null")
 	@Test
 	void test4() {
-		
+		assertThrows(NullPointerException.class, () -> {
+			account.addGame(null);
+		});
 	}
 	
 	@DisplayName("addGame succesfully adds new game into account")
 	@Test
 	void test5() {
+		Game game = new Game(null, null, null, null);
+		account.addGame(game);
+		assertNotNull(game);
+		assertEquals(game, account.getSavedGames().get(account.getSavedGames().size() - 1));
 		
+		Game game2 = new Game(null, null, null, null);
+		account.addGame(game2);
+		assertNotNull(game2);
+		assertEquals(game2, account.getSavedGames().get(account.getSavedGames().size() - 1));
+		
+		Game game3 = new Game(null, null, null, null);
+		account.addGame(game3);
+		assertNotNull(game3);
+		assertEquals(game3, account.getSavedGames().get(account.getSavedGames().size() - 1));
 	}
 
 }
