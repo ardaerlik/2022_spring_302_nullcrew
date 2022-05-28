0e96916aad5c4ee78e828acb2ae2d97ec0fba872
Arda Erlik <aerlik20@ku.edu.tr>
2022-05-29 00:58:30 +0300

    update User and Database

--- app/src/main/java/com/nullcrew/Utilities/Database.java
+++ app/src/main/java/com/nullcrew/Utilities/Database.java
@@ -1,16 +1,14 @@
 package com.nullcrew.Utilities;
 
+import java.util.ArrayList;
+
 import com.nullcrew.Domain.Models.Game;
 import com.nullcrew.Domain.Models.User;
 
 public interface Database {
 	public abstract void saveTheGame(Game game);
 	
-	public abstract void loadTheGames();
-	
-	public abstract void setUser(User user);
-	
-	public abstract User getUser();
+	public abstract ArrayList<Game> loadTheGames();
 	
 	public abstract void subscribeSaveLoadObserver(SaveLoadObserver observer);
 	
