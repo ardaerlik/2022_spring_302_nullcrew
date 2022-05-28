cf505b24547d0c360f0ddaece36e69ffd926f683
Arda Erlik <aerlik20@ku.edu.tr>
2022-05-29 00:28:06 +0300

    create DatabaseAdapter

---
+++ app/src/main/java/com/nullcrew/Utilities/DatabaseAdapter.java
@@ -0,0 +1,87 @@
+package com.nullcrew.Utilities;
+
+import com.nullcrew.Domain.Models.Game;
+import com.nullcrew.Domain.Models.User;
+
+public class DatabaseAdapter implements Database {
+	private DBManager dbManager;
+
+	public DatabaseAdapter(DBManager dbManager) {
+		this.dbManager = dbManager;
+	}
+
+	@Override
+	public void saveTheGame(Game game) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loadTheGames() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void registerUser(String email, String password, String forgotKey) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void loginUser(String email, String password) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void resetPassword(String email, String newPassword, String forgotKey) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public User getUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void subscribeAuthObserver(AuthObserver observer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifyAuthObservers(String response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void subscribeSaveLoadObserver(SaveLoadObserver observer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void notifySaveLoadObserver(String response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public DBManager getDbManager() {
+		return dbManager;
+	}
+
+	public void setDbManager(DBManager dbManager) {
+		this.dbManager = dbManager;
+	}
+
+}
